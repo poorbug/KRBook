c12f7f8ab738beba9ff06df6300676ce4dd3dc41
libaixiang <libaixiang@bytedance.com>
2017-10-25 20:37:09 +0800

    initial
    Change-Id: I367ebfdcd6e2c404746d201fd9208de9bd2da01e

---
+++ data/heros.js
@@ -0,0 +1,27 @@
+module.exports = [
+  {
+    img: "http://omhr7p9e5.bkt.clouddn.com/microapp/krbook/heros.png",
+    desc: "123123",
+    attr: [7, 8, 0, 3]
+  },
+  {
+    img: "http://omhr7p9e5.bkt.clouddn.com/microapp/krbook/heros.png",
+    desc: "123123",
+    attr: [7, 8, 0, 3]
+  },
+  {
+    img: "http://omhr7p9e5.bkt.clouddn.com/microapp/krbook/heros.png",
+    desc: "123123",
+    attr: [7, 8, 0, 3]
+  },
+  {
+    img: "http://omhr7p9e5.bkt.clouddn.com/microapp/krbook/heros.png",
+    desc: "123123",
+    attr: [7, 8, 0, 3]
+  },
+  {
+    img: "http://omhr7p9e5.bkt.clouddn.com/microapp/krbook/heros.png",
+    desc: "123",
+    attr: [7, 8, 0, 3]
+  }
+]
---
+++ pages/hero/hero.js
@@ -0,0 +1,7 @@
+Page({
+  data: {
+    heros: require('../../data/heros')
+  },
+  onLoad: function () {
+  }
+})
