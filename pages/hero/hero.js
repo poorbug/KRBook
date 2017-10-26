d207694b7be6e24d2054dcc014ecc43a183a3cbd
libaixiang <libaixiang@bytedance.com>
2017-10-26 20:09:50 +0800

    [ Hero ] desc
    Change-Id: I69502ca9bb38d6e98b66e7a6fe7091021034bda5

--- pages/hero/hero.js
+++ pages/hero/hero.js
@@ -1,7 +1,13 @@
 Page({
   data: {
-    heros: require('../../data/heros')
+    heros: require('../../data/heros'),
+    show: -1
   },
   onLoad: function () {
+  },
+  toggle: function (e) {
+    this.setData({
+      show: e.target.dataset.idx === this.data.show ? -1 : e.target.dataset.idx
+    })
   }
 })
