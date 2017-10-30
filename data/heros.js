84a1ed7a88ca6bb01b3d2cfed680efb1a81d725e
libaixiang <libaixiang@bytedance.com>
2017-10-30 20:24:02 +0800

    [ gif ]
    Change-Id: I063196226e6fce4a5f1a7f1b7346f50a595fe2e6

--- data/heros.js
+++ data/heros.js
@@ -2,61 +2,82 @@ module.exports = [
   {
     img: "http://omhr7p9e5.bkt.clouddn.com/microapp/krbook/hero-0.jpg",
     desc: "正义的守护者，黑暗的惩戒者，光之追寻者杰拉尔德堪称利尼维亚军队最杰出的任务！",
-    attr: [8, 6, 0, 5]
+    attr: [8, 6, 0, 5],
+    name: 'Holy-Paladin',
+    audio: [
+      'Prepare for glory.',
+      'Honor guide me.',
+      'I will cleanse this land.',
+      'Come on. You wanner live forever?',
+      'Ah~'
+    ]
   },
   {
     img: "http://omhr7p9e5.bkt.clouddn.com/microapp/krbook/hero-1.jpg",
+    animal: true,
     desc: "寂静如黑夜，致命而美丽。倾倒在她魅力之下者多不胜数，而亡命于她箭下者更是不计其数。",
-    attr: [3, 3, 6, 6]
+    attr: [3, 3, 6, 6],
+    name: ''
   },
   {
     img: "http://omhr7p9e5.bkt.clouddn.com/microapp/krbook/hero-2.jpg",
     desc: "一位英雄，拥有无可匹敌的力量和坚不可摧的意志。以勇往直前的态度，施展毁天灭地力量！",
-    attr: [8, 7, 0, 4]
+    attr: [8, 7, 0, 4],
+    name: ''
   },
   {
     img: "http://omhr7p9e5.bkt.clouddn.com/microapp/krbook/hero-3.jpg",
     desc: "释放枪林弹雨，布设地雷矩阵，敌人哪敢越雷池半步！",
-    attr: [6, 5, 5, 4]
+    attr: [6, 5, 5, 4],
+    name: ''
   },
   {
     img: "http://omhr7p9e5.bkt.clouddn.com/microapp/krbook/hero-4.jpg",
     desc: "解除利尼维亚的奥术力量禁制，释放神秘风暴，镇压你的敌人！",
-    attr: [2, 2, 8, 8]
+    attr: [2, 2, 8, 8],
+    name: ''
   },
   {
     img: "http://omhr7p9e5.bkt.clouddn.com/microapp/krbook/hero-5.jpg",
     desc: "以群星烈火焚尽王国的敌人！",
-    attr: [6, 8, 0, 6]
+    attr: [6, 8, 0, 6],
+    name: ''
   },
   {
     img: "http://omhr7p9e5.bkt.clouddn.com/microapp/krbook/hero-6.jpg",
     desc: "陛下便是为战场带来力量与勇气的源泉。国王万岁！",
-    attr: [5, 6, 6, 3]
+    attr: [5, 6, 6, 3],
+    name: ''
   },
   {
     img: "http://omhr7p9e5.bkt.clouddn.com/microapp/krbook/hero-7.jpg",
     desc: "她精通寒冰，却也极度无情，但凡有阻挡者，都会被她冻成冰雕，击个粉碎！",
-    attr: [3, 2, 8, 7]
+    attr: [3, 2, 8, 7],
+    name: ''
   },
   {
     img: "http://omhr7p9e5.bkt.clouddn.com/microapp/krbook/hero-8.jpg",
     desc: "来自北方的战士，掌控巨熊之力和先祖智慧！",
-    attr: [8, 8, 0, 5]
+    attr: [8, 8, 0, 5],
+    name: ''
   },
   {
     img: "http://omhr7p9e5.bkt.clouddn.com/microapp/krbook/hero-9.jpg",
     desc: "在侏儒发明家眼里，巨大的橡树与庞大的敌人没有什么区别。",
-    attr: [8, 7, 0, 3]
+    animal: true,
+    attr: [8, 7, 0, 3],
+    name: ''
   },
   {
     img: "http://omhr7p9e5.bkt.clouddn.com/microapp/krbook/hero-10.jpg",
     desc: "冷酷无情的武士，不知疲倦地寻求复仇，身着黑暗荣誉武士甲，手执烈焰熊熊武士刀。",
-    attr: [7, 8, 0, 6]
+    attr: [7, 8, 0, 6],
+    name: ''
   },
   {
     img: "http://omhr7p9e5.bkt.clouddn.com/microapp/krbook/hero-11.jpg",
     desc: "这位身材高达的半神，肉身由雷电构成，挥舞着一把附有雷电之力的战锤。",
-    attr: [7, 8, 0, 5]
+    attr: [7, 8, 0, 5],
+    name: ''
   }
 ]
--- pages/hero/hero.js
+++ pages/hero/hero.js
@@ -1,13 +1,33 @@
 Page({
   data: {
     heros: require('../../data/heros'),
-    show: -1
+    show: -1,
+    pos: 0
   },
   onLoad: function () {
   },
-  toggle: function (e) {
+  toggle: function () {
+    const { pos, show } = this.data
     this.setData({
-      show: e.currentTarget.dataset.idx === this.data.show ? -1 : e.currentTarget.dataset.idx
+      show: pos === show ? -1 : pos
+    })
+  },
+  swipe: function (e) {
+    this.setData({
+      pos: e.detail.current,
+      show: -1
+    })
+  },
+  playAudio: function () {
+    console.log('playAudio')
+    const { pos, heros } = this.data
+    const idx = Math.floor(Math.random() * heros[pos].audio.length)
+    wx.playBackgroundAudio({
+      dataUrl: `http://omhr7p9e5.bkt.clouddn.com/microapp/krbook/${heros[pos].name}-${idx}.caf`,
+    })
+    wx.showToast({
+      title: heros[pos].audio[idx],
+      duration: 2000
     })
   },
   onShareAppMessage: function () {
