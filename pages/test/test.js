59e0c81890720efe0d444d3ab26e6b874bf7f3c1
libaixiang <libaixiang@bytedance.com>
2017-10-30 09:25:21 +0800

    [ msgboard ] 组件化
    Change-Id: Id70b27139790a134f53b2a0dc2d3b1cc727f70a5

--- pages/test/test.js
+++ pages/test/test.js
@@ -1,4 +1,9 @@
+import { Msgboard } from '../../templates/msgboard/msgboard'
 Page({
   data: {
+    msg: { isHide: true }
+  },
+  onLoad: function () {
+    new Msgboard()
   }
 })
--- templates/msgboard/msgboard.js
+++ templates/msgboard/msgboard.js
@@ -1,11 +1,38 @@
-Page({
-  data: {
-    show: true
+let _comData = {
+  'msg.isHide': true
+}
+
+// let _comEvent = {
+//   msg_isShow: function () {
+//     this.msgShow()
+//   },
+//   msg_isHide: function () {
+//     this.msgHide()
+//   }
+// }
+
+let _comMethod = {
+  msgShow: function () {
+    this.setData({ 'msg.isHide': false })
   },
-  toggle: function () {
-    console.log(11111)
-    this.setData({
-      show: !this.data.show
-    })
+  msgHide: function () {
+    this.setData({ 'msg.isHide': true })
   }
-})
+}
+
+function Msgboard() {
+  console.log(this)
+  let pages = getCurrentPages()
+  let curPage = pages[pages.length - 1]
+  //组件中调用页面
+  this._page = curPage
+  Object.assign(curPage,/* _comEvent,*/ _comMethod)
+  curPage.setData(_comData)
+
+  curPage.msgboard = this
+
+  console.log(this)
+  return this
+}
+
+export { Msgboard }
