561cbc33c7cd82cad65c3c3ee837660a87e40cd5
libaixiang <libaixiang@bytedance.com>
2017-11-02 10:53:49 +0800

    [ Hero ] git & audio
    Change-Id: If157d99c79a7b2e1c5970c45967c59037dcf70b1

--- data/heros.js
+++ data/heros.js
@@ -30,7 +30,7 @@ module.exports = [
     img: "http://omhr7p9e5.bkt.clouddn.com/microapp/krbook/hero-2.jpg",
     desc: "一位英雄，拥有无可匹敌的力量和坚不可摧的意志。以勇往直前的态度，施展毁天灭地力量！",
     attr: [8, 7, 0, 4],
-    name: '',
+    name: 'Malik-Hammerfury',
     audio: [
       'It\'s hammer time!',
       'Can\'t touch this!',
@@ -43,7 +43,7 @@ module.exports = [
     img: "http://omhr7p9e5.bkt.clouddn.com/microapp/krbook/hero-3.jpg",
     desc: "释放枪林弹雨，布设地雷矩阵，敌人哪敢越雷池半步！",
     attr: [6, 5, 5, 4],
-    name: '',
+    name: 'Bolin-Farslayer',
     audio: [
       'Weapons ready!',
       'I never miss!',
@@ -56,12 +56,12 @@ module.exports = [
     img: "http://omhr7p9e5.bkt.clouddn.com/microapp/krbook/hero-4.jpg",
     desc: "解除利尼维亚的奥术力量禁制，释放神秘风暴，镇压你的敌人！",
     attr: [2, 2, 8, 8],
-    name: '',
+    name: 'Magnus-Spellbane',
     audio: [
-      'Hocus pocus.',
-      'Knowlege is power.',
-      'Power, unlimited power!',
       'It\'s a kind of magic.',
+      'Hocus pocus.',
+      'Power, unlimited power!',
+      'Knowlege is power.',
       'No...',
     ]
   },
@@ -69,7 +69,7 @@ module.exports = [
     img: "http://omhr7p9e5.bkt.clouddn.com/microapp/krbook/hero-5.jpg",
     desc: "以群星烈火焚尽王国的敌人！",
     attr: [6, 8, 0, 6],
-    name: '',
+    name: 'Ignus',
     audio: [
       'Let\'s burn!',
       'Need a light?',
@@ -82,28 +82,28 @@ module.exports = [
     img: "http://omhr7p9e5.bkt.clouddn.com/microapp/krbook/hero-6.jpg",
     desc: "陛下便是为战场带来力量与勇气的源泉。国王万岁！",
     attr: [5, 6, 6, 3],
-    name: '',
+    name: 'King-Denas',
     audio: [
-      'Bow... to the KING!',
+      'Attack!!',
+      '!@#!#!$$%???',
+      'For me!!!',
       'Go ahead, make my day!',
       'Get off my land.',
-      'For me!!!',
-      'garbled babbling.',
-      'You slacker!',
-      'Attack!!',
+      'Bow... to the KING!',
       'Ahhh...',
+      '???'
     ]
   },
   {
     img: "http://omhr7p9e5.bkt.clouddn.com/microapp/krbook/hero-7.jpg",
     desc: "她精通寒冰，却也极度无情，但凡有阻挡者，都会被她冻成冰雕，击个粉碎！",
     attr: [3, 2, 8, 7],
-    name: '',
+    name: 'Elora-Wintersong',
     audio: [
-      'Ice, ice, baby!',
       'Winter is coming!',
       'It\'s chilling time!',
       'Cool!',
+      'Ice, ice, baby!',
       'Nooo...',
     ]
   },
@@ -111,12 +111,12 @@ module.exports = [
     img: "http://omhr7p9e5.bkt.clouddn.com/microapp/krbook/hero-8.jpg",
     desc: "来自北方的战士，掌控巨熊之力和先祖智慧！",
     attr: [8, 8, 0, 5],
-    name: '',
+    name: 'Ingvar-Bearclaw',
     audio: [
-      'Until Ragnarök!',
       'Valhalla waits!',
       'You won\'t like me when I\'m angry.',
       'Bear with me!',
+      'Until Ragnarök!',
       'Yahhh...',
     ]
   },
@@ -125,7 +125,7 @@ module.exports = [
     desc: "在侏儒发明家眼里，巨大的橡树与庞大的敌人没有什么区别。",
     animal: true,
     attr: [8, 7, 0, 3],
-    name: '',
+    name: 'Hacksaw',
     audio: [
       'Timber! Hoo hoo haa haa!',
       'Cowabunga!',
@@ -138,12 +138,12 @@ module.exports = [
     img: "http://omhr7p9e5.bkt.clouddn.com/microapp/krbook/hero-10.jpg",
     desc: "冷酷无情的武士，不知疲倦地寻求复仇，身着黑暗荣誉武士甲，手执烈焰熊熊武士刀。",
     attr: [7, 8, 0, 6],
-    name: '',
+    name: 'Oni',
     audio: [
       'Off with their heads!',
-      'Banzai!',
-      'My blades are thirsty!',
       'A good death!',
+      'My blades are thirsty!',
+      'Banzai!',
       'Death brings me honour...',
     ]
   },
@@ -151,7 +151,7 @@ module.exports = [
     img: "http://omhr7p9e5.bkt.clouddn.com/microapp/krbook/hero-11.jpg",
     desc: "这位身材高达的半神，肉身由雷电构成，挥舞着一把附有雷电之力的战锤。",
     attr: [7, 8, 0, 5],
-    name: '',
+    name: 'Thor',
     audio: [
       'For Midgard!',
       'I say thee nay, vile one!',
